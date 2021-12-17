5052a4a0f08108c7bed7721ec24b57918fd73b52
Dejan Brnic <dejan.brnic@aerdata.com>
2021-12-17 14:07:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Simple Copyright Add README
+
+## Features
+
+Use it for adding very simple Copyright info to your files. It adds copyright info to opened file only.
+
+example:
+
+/* <br>
+Place your copyright text here <br>
+*/
+
+## Extension Settings
+
+To add your custom Copyright text, please go into extension settings and update the Copyright Text field.
+
+For example:
+
+This extension contributes the following settings:
+
+* `simpleCopyrightAdd.copyrightText`: Put your Copyright text here
+
+## Usage
+
+In open file press `CTRL + SHIFT + P` and type: Copyright insert
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of extension
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,61 @@
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	console.log('Congratulations, your extension "simple-copyright-add" is now active!');
+
+	let disposable = vscode.commands.registerCommand('simple-copyright-add.copyRightInsert', function () {
+
+		const configView = vscode.workspace.getConfiguration();
+		
+		let commentStart = '';
+		let commentEnd = '';
+		let copyrightText = configView.get("simpleCopyrightAdd.copyrightText") || "Place your copyright info here";
+
+		let copyrightPosition = new vscode.Position(0, 0);
+		const editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			vscode.window.showInformationMessage('No editor is active')
+			return
+		}
+
+		fileExtension = editor.document.uri.fsPath.split('.').pop();
+		switch (fileExtension.toLowerCase()) {
+			case 'js': commentStart = '/*\n'; commentEnd='\n*/\n'; break;
+			case 'ts': commentStart = '/*\n'; commentEnd='\n*/\n'; break;
+			case 'java': commentStart = '/*\n'; commentEnd='\n*/\n'; break;
+			case 'c': commentStart = '/*\n'; commentEnd='\n*/\n'; break;
+			case 'cpp': commentStart = '/*\n'; commentEnd='\n*/\n'; break;
+			case 'go': commentStart = '/*\n'; commentEnd='\n*/\n'; break;
+			case 'css': commentStart = '/*\n'; commentEnd='\n*/\n'; break;
+			case 'html': commentStart = '<!--\n'; commentEnd='\n-->\n'; break;
+			case 'py': commentStart = '\"\"\"\n'; commentEnd='\n\"\"\"\n'; break;
+			case 'txt': commentStart = '\n'; commentEnd='\n'; break;
+			default: commentStart = '/*\n'; commentEnd='\n*/\n';
+		}
+
+		if (fileExtension != '') {
+			editor.edit( instance => {
+				instance.insert(copyrightPosition, commentStart + copyrightText + commentEnd);
+			});
+			vscode.window.showInformationMessage(`Copyright is added to ${fileExtension}`);
+		} else {
+			vscode.window.showInformationMessage('File not Supported yet');
+		}
+
+
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
